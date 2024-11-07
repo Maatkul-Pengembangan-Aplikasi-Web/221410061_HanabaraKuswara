99334bf74acf7cfe753dfcb433b72a11b8a6dd63
Hana-bara <hanabara42@gmail.com>
2024-11-07 20:22:29 +0700

    +Fungsi Hapus Data Delete

--- app/Http/Controllers/ProdiController.php
+++ app/Http/Controllers/ProdiController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Models\Prodi;
-
 use Illuminate\Http\Request;
 
 class ProdiController extends Controller
@@ -11,24 +10,44 @@ class ProdiController extends Controller
     public function index()
     {
         $prodis = Prodi::orderBy('id', 'desc')->get();
-    return view('prodi.index', compact('prodis'));
-}
+        return view('prodi.index', compact('prodis'));
+    }
 
-public function create()
-{
-    return view('prodi.create');
-}
+    public function create()
+    {
+        return view('prodi.create');
+    }
 
-public function save(Request $request)
-{
-    $request->validate([
-        'nama' => 'required'
-    ]);
+    public function save(Request $request)
+    {
+        $request->validate([
+            'nama' => 'required'
+        ]);
 
-    Prodi::create([
-        'nama' => $request->nama
-    ]);
+        Prodi::create([
+            'nama' => $request->nama
+        ]);
 
-    return redirect()->route('/prodi')->with('success', 'Program Studi berhasil ditambahkan');
+        return redirect()->route('/prodi')->with('success', 'Program Studi berhasil ditambahkan');
+    }
+
+    public function edit($id)
+    {
+        $prodi = Prodi::findOrFail($id);
+        return view('prodi.edit', compact('prodi'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'nama' => 'required'
+        ]);
+
+        $prodi = Prodi::findOrFail($id);
+        $prodi->update([
+            'nama' => $request->nama
+        ]);
+
+        return redirect()->route('/prodi')->with('success', 'Program Studi berhasil diupdated');
     }
 }
