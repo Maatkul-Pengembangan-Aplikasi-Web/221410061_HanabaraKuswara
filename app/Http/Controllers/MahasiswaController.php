059f72916f1da42f994a2b4fefba731432006451
Hana-bara <hanabara42@gmail.com>
2024-12-16 14:27:00 +0700

    update error edit

--- app/Http/Controllers/MahasiswaController.php
+++ app/Http/Controllers/MahasiswaController.php
@@ -3,8 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Mahasiswa;
-use App\Models\Prodi;
 use Illuminate\Http\Request;
+use App\Models\Prodi;
+
 
 class MahasiswaController extends Controller
 {
@@ -18,6 +19,7 @@ class MahasiswaController extends Controller
 
         return view('mahasiswa.index', compact('mahasiswas', 'search'));
     }
+
     /**
      * Show the form for creating a new resource.
      */
@@ -45,7 +47,7 @@ class MahasiswaController extends Controller
         $mahasiswa = Mahasiswa::create($validation);
 
         if ($mahasiswa) {
-            session()->flash('success', 'Data Mahasiswa Berhasil di Tambahkan');
+            session()->flash('success', 'Data Mahasiswa Berhasil di Masukkan');
             return redirect(route('/mahasiswa'));
         } else {
             session()->flash('error', 'Ada Kesalahan');
@@ -96,13 +98,12 @@ class MahasiswaController extends Controller
 
         // Proses upload gambar baru
         if ($request->hasFile('foto')) {
-            $namaFoto = $request->npm . '.' . $request->foto->extension();
-            $request->foto->move(public_path('fotomahasiswa'), $namaFoto);
-
             // Hapus gambar lama jika ada
             if ($mahasiswa->foto && file_exists(public_path('fotomahasiswa/' . $mahasiswa->foto))) {
                 unlink(public_path('fotomahasiswa/' . $mahasiswa->foto));
             }
+            $namaFoto = $request->npm . '.' . $request->foto->extension();
+            $request->foto->move(public_path('fotomahasiswa'), $namaFoto);
 
             // Set gambar baru
             $mahasiswa->foto = $namaFoto;
@@ -119,6 +120,11 @@ class MahasiswaController extends Controller
         return redirect(route('/mahasiswa'));
     }
 
+
+    /**
+     * Update the specified resource in storage.
+     */
+   
 
     /**
      * Remove the specified resource from storage.
